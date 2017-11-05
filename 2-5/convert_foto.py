a84026c0e90cb47668c2a8c36409b9e31089b562
Ilya Anichkin <xpapapapokerfacex@gmail.com>
2017-11-05 15:55:10 +0300

    Update convert_foto.py

--- 2-5/convert_foto.py
+++ 2-5/convert_foto.py
@@ -3,17 +3,14 @@ import subprocess
 
 source = 'Source'
 current_dir = os.path.dirname(os.path.abspath(__file__))
-process = subprocess.run('convert')
-print(process.args)
 if __name__ == '__main__':
-    print(current_dir)
-    images = os.listdir(os.path.join(current_dir, source))
     images_dir = os.path.join(current_dir, source)
-    result = os.path.join(current_dir, 'Result')
-    for convert_foto, image in enumerate(images):
-        image_dir = os.path.join(images_dir, image)
-        result_image_dir = os.path.join(result, image)
-        print(image_dir)
-        print(result_image_dir)
-        process = subprocess.run('convert image_dir  -resize 200 result_image_dir')
-
+    images = os.listdir(images_dir)
+    result_dir = os.path.join(current_dir, 'Result')
+    if os.path.exists(result_dir) == False:
+        os.mkdir(os.path.join(current_dir, 'Result'))
+    for image in images:
+        image_dir = (os.path.join(images_dir, image)).replace('\\','/')
+        image_result_dir = (os.path.join(result_dir, image)).replace('\\','/')
+        args_for_convert = ('convert ' + image_dir + ' -resize 200 ' + image_result_dir)
+        process = subprocess.run(args_for_convert)
